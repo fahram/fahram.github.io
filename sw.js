74e793412c2a9e4c6cc4a101f4a80e88e80d0eed
Khaidir Fahram <khaidir@fahram.com>
2021-07-08 15:36:14 +0700

    revisi 1

--- sw.js
+++ sw.js
@@ -5,8 +5,6 @@ var serviceWorkerOption = {
     "/bundle.js",
     "/favicon.ico",
     "/manifest.json",
-    "/img/hero.jpg",
-    "/img/placeholder.jpeg",
     "/icons/icon-128x128.png",
     "/icons/icon-144x144.png",
     "/icons/icon-152x152.png",
@@ -16,6 +14,8 @@ var serviceWorkerOption = {
     "/icons/icon-512x512.png",
     "/icons/icon-72x72.png",
     "/icons/icon-96x96.png",
+    "/img/hero.jpg",
+    "/img/placeholder.jpeg",
     "/index.html"
   ]
 };
